65568186ab8683fb2cc7b383b186a5c2a3567c92
WoIIo <roelswolf@gmail.com>
2021-09-10 13:41:55 +0200

    db naam veranderd

--- connect/connect.php
+++ connect/connect.php
@@ -3,7 +3,7 @@
     $server_name = "localhost";
     $server_user = "root";
     $server_password = "wolfroels";
-    $db_name = "krisdb";
+    $db_name = "krisDB";
     $conn = new mysqli($server_name, $server_user, $server_password, $db_name);
     if($conn -> connect_error){
         die("connection failed: " . $conn -> connect_error);
